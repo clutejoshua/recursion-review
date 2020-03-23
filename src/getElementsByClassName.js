da9f6119eb43c29e732cee81fc5d31ee02e901a3
JORDAN CHUN <JCHUN765@GMAIL.COM>
2020-03-23 16:48:09 -0700

    Completed getElementsbyClassName. passed testing suites.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  let result = [];
+  let node = document.body;
+  var recurse = function(element) {
+    let classList = element.classList;
+    if (classList && classList.contains(className)) {
+      result.push(element);
+    }
+    if (node.hasChildNodes()) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        recurse(element.childNodes[i]);
+      }
+    }
+  };
+  recurse(node);
+  return result;
 };
+
+
+// var getElementsByClassName = function(className) {
+//   // your code here
+//   let result = [];
+//   let node = document.body;
+//   function recurse (element) {
+//     let classList = element.classList
+//     if (classList && classList.contains(className)) {
+//       result.push(element);
+//     }
+//     if(element.childNodes !== undefined) {
+//       for (let node in element.childNodes) {
+//         recurse(element.childNodes[node]);
+//         }
+//       }
+//     }
+//     recurse(node);
+//     return result;
+// };
